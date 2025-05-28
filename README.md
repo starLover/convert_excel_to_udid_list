a2456ef883d18a93c13ffafbb2f908bd4932c313
niexinxin <1498146887@qq.com>
2025-05-28 10:47:19 +0800

    excel转udid list

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # convert_excel_to_udid_list
 批量处理excel统计的udid生成苹果设备列表
+
+### 使用步骤 (Usage)
+##### 1. 使用文件UDID.xlsx统计UDID，设备类型不含mac可统一填写ios(可通过pgyer等三方工具获取设备的UDID)，按照文档格式，请勿修改文件名和表单名。
+##### 2. 确保已经安装了 pandas 和 openpyxl 库， 用于处理excel表格。可以使用以下命令进行安装(``已安装可跳过本步骤``):
+```
+pip install pandas openpyxl
+```
+##### 3. 确保excel和脚本在同一目录下，执行以下命令:
+```
+python excel_to_udid_list.py
+```
+成功后可看到 ，``multiple-device-upload.txt`` 文件就是可以上传
+```
+➜  convert_excel_to_udid_list git:(main) ✗ python excel_to_udid_list.py
+成功将 Excel 文件 'UDID.xlsx' 转换为 TXT 文件 'multiple-device-upload.txt'
+```
+##### 4.  上传 multiple-device-upload.txt 文件即可大功告成!
+![image.png](https://upload-images.jianshu.io/upload_images/3474734-6de93e30166ca650.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+### Enjoy your day! 🤪
---
+++ excel_to_udid_list.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+# 定义 Excel 文件路径和工作表名称
+excel_file_path = 'UDID.xlsx'
+sheet_name = 'UDID'  # 指定要读取的工作表名称
+
+try:
+    # 读取 Excel 文件
+    df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+    # 交换第一第二两列数据
+    df.iloc[:, [0, 1]] = df.iloc[:, [1, 0]].values
+
+    # 定义输出 TXT 文件的路径
+    txt_file_path = 'multiple-device-upload.txt'
+
+    # 打开 TXT 文件以写入数据
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
+        # 遍历 DataFrame 的每一行
+        line = "Device ID\tDevice Name\tDevice Platform"
+        file.write(line + '\n')
+        for index, row in df.iterrows():
+            if row.isnull().any():
+                print(f"第 {index} 行存在空值: {row.values} ")
+                continue
+            # 将每行数据转换为制表符分隔的字符串
+            line = '\t'.join(map(str, row.values))
+            # 将处理后的行写入文件，并添加换行符
+            file.write(line + '\n')
+
+    print(f"成功将 Excel 文件 '{excel_file_path}' 转换为 TXT 文件 '{txt_file_path}'")
+
+except FileNotFoundError:
+    print("文件不存在，请检查路径是否正确。")
+except Exception as e:
+    print(f"发生错误：{e}")
